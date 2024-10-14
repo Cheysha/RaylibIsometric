7c32bb18065fea517fae4825694dab11b399b764
Chey <hyman.sha95@icloud.com>
2024-10-13 20:37:16 -0500

    worked on battle mechanic

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ class Program
 {
     public const int MAPWIDTH = 14; 
     public const int MAPHEIGHT = 20; 
-     /* Y IS UP, Z IS FOWARD*/
 
     public static int screenWidth = 1280;
     public static int screenHeight = 720;
@@ -24,8 +23,8 @@ class Program
 
     public static int[,] gameMap = new int[MAPHEIGHT, MAPWIDTH]{
         {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1},
-        {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
         {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -61,14 +60,13 @@ class Program
         
         // Set up Camera
         Camera3D camera = new Camera3D();
-        camera.target = new Vector3(0,0,0);
-        camera.up = new Vector3(0.0f, 1.0f, 0.0f);
+        camera.up = new Vector3(0.0f, 1.0f, 0.0f); // this makes y up!!
         camera.projection = (int)CameraProjection.CAMERA_PERSPECTIVE;
         camera.fovy = 45;
            
         // Setting up background
         Image bg;
-        int animFrames = 0;
+        int animFrames;
         int currentAnimFrame = 0;
         int frameDelayCycles = 4;
         int delayCounter = 0;
@@ -99,22 +97,23 @@ class Program
         */
         while (!Raylib.WindowShouldClose())
         {
+            if(Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE)){battling = false;} // failsafe to stop battle
+
+            // Update the main game window
             if (!battling){
                 Raylib.UpdateMusicStream(backgroundMusic);
 
                 // Update bg
                 delayCounter++;
-                if (delayCounter >= frameDelayCycles)
-                {
+                if (delayCounter >= frameDelayCycles){
                     currentAnimFrame++;
-
                     if (currentAnimFrame >= animFrames) 
                         currentAnimFrame = 0;
-
                     nextFrameDataOffset = (uint)(bg.width*bg.height*currentAnimFrame);
                     unsafe{ Raylib.UpdateTexture(bgtexture, (uint*)bg.data + nextFrameDataOffset); }
                     delayCounter = 0;
                 }
+
                 // Update Input 
                 var cahrMovementVector = Vector2.Zero;
                 if (Raylib.IsKeyDown(KeyboardKey.KEY_Q)) 
@@ -122,23 +121,17 @@ class Program
                 if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) 
                     camera.position.Y -= cameraSpeed;
                 if (Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT)) 
-                    cahrMovementVector.X = -playerSpeed;
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT)) 
-                    cahrMovementVector.X = playerSpeed;
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP)) 
                     cahrMovementVector.Y = -playerSpeed;
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN)) 
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT)) 
                     cahrMovementVector.Y = playerSpeed;
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP)) 
+                    cahrMovementVector.X = playerSpeed;
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN)) 
+                    cahrMovementVector.X = -playerSpeed;
 
                 // Update State
                 if(cahrMovementVector != Vector2.Zero){
-                    //consider camera here
-                    Vector3 cameraForward = Vector3.Normalize(camera.target - camera.position);
-                    Vector3 cameraRight = Vector3.Normalize(Vector3.Cross(cameraForward, new Vector3(0, 1, 0)));
-
-                    Vector2 movementDirection = new Vector2(cameraForward.X, cameraForward.Z) * cahrMovementVector.Y + new Vector2(cameraRight.X, cameraRight.Z) * cahrMovementVector.X;
-                   // var charPos2 = new Vector2(charPos.X + cahrMovementVector.X, charPos.Y + cahrMovementVector.Y);
-                    var charPos2 = new Vector2(charPos.X + movementDirection.X, charPos.Y + movementDirection.Y);
+                    var charPos2 = new Vector2(charPos.X + cahrMovementVector.X, charPos.Y + cahrMovementVector.Y);
                     var emnPos2 = new Vector2(emnPos.X, emnPos.Y);
 
                     if (charPos2.X >= 0 && charPos2.X <= MAPWIDTH && charPos2.Y >= 0 && charPos2.Y <= MAPHEIGHT){// keep char in bounds
@@ -147,35 +140,34 @@ class Program
                                 charPos.X = charPos2.X;
                                 charPos.Y = charPos2.Y;
                             }
-                        else battle();
+                        else battling = true;
                         }
                     } 
                 }
                 camera.target = new Vector3(charPos.X,zOffset,charPos.Y);
             } // END NOT BATTLE
             
+            // Update the Battle Window
             if(battling){
-                //battle sequence
+
             }
-
-
-
-
 
             /*
                 DRAW
             */
+
+            // Render 
             Raylib.BeginTextureMode(renderTexture);
-            
-            // draw background, this can be changed if want different style
+
             Raylib.DrawTexturePro(  texture: bgtexture,
                                     source: new Rectangle(0,0,scaledWidth,-scaledHeight),
                                     dest: new Rectangle(0,0,screenWidth,screenHeight),
                                     origin: new Vector2(0,0),
                                     rotation:0f, tint: CustomColors.WHITE); 
+                                    
             Raylib.ClearBackground(CustomColors.WHITE);
             
-            // Draw Models, Map, Axis
+            // Render Models, Map, Axis
             Raylib.BeginMode3D(camera);
 
             Raylib.DrawModel(   model:charModel,
@@ -186,19 +178,8 @@ class Program
                                 scale: 1.0f, tint: CustomColors.RED);
 
             drawMap();
-                // X axis
-            Raylib.DrawLine3D(  startPos: new Vector3(0, 0, 0),
-                                endPos:  new Vector3(axisLength, 0, 0), 
-                                color: xAxisColor); 
-                // Y axis
-            Raylib.DrawLine3D(  startPos: new Vector3(0, 0, 0),
-                                endPos: new Vector3(0, axisLength, 0),
-                                color: yAxisColor); 
-                // Z axis
-            Raylib.DrawLine3D(  startPos: new Vector3(0, 0, 0), 
-                                endPos: new Vector3(0, 0, axisLength),
-                                color: zAxisColor); 
-     
+            drawAxisLines();
+
             Raylib.EndMode3D();
             Raylib.EndTextureMode();
 
@@ -210,14 +191,18 @@ class Program
                                     origin: new Vector2(0,0),
                                     rotation: 0f,
                                     tint: CustomColors.WHITE);
-
+            
+            // Draw the battle window
+            if(battling){
+            Raylib.DrawRectangleV(  position: new Vector2(screenWidth /2 - screenWidth /4, screenHeight /2 - screenHeight /4),
+                                    size: new Vector2(screenWidth /2, screenHeight /2),
+                                    color: CustomColors.BLUE
+            );
+            }
+            
             // DRAW DEBUG
             Raylib.DrawText(text: $"{charPos}", posX: 0, posY: 0, fontSize: 20, color: CustomColors.WHITE);
             Raylib.EndDrawing();
-            
-            if(battling){
-                //draw battle sequence
-            }
         }
         Raylib.CloseWindow();
 
@@ -235,14 +220,22 @@ class Program
                 }
             }
         }
-        
-        void battle(){
-            //battling = true;
-            Console.WriteLine("Battle,");
+        void drawAxisLines(){
+                // X axis
+            Raylib.DrawLine3D(  startPos: new Vector3(0, 0, 0),
+                                endPos:  new Vector3(axisLength, 0, 0), 
+                                color: xAxisColor); 
+                // Y axis
+            Raylib.DrawLine3D(  startPos: new Vector3(0, 0, 0),
+                                endPos: new Vector3(0, axisLength, 0),
+                                color: yAxisColor); 
+                // Z axis
+            Raylib.DrawLine3D(  startPos: new Vector3(0, 0, 0), 
+                                endPos: new Vector3(0, 0, axisLength),
+                                color: zAxisColor); 
         }
-
-        bool checkFloorTile(Vector2 position){ // should probaly just use 'Points'
-            if(gameMap[(int)position.Y,(int)position.X ] ==1)
+        bool checkFloorTile(Vector2 position){
+            if(gameMap[(int)position.Y,(int)position.X ] == 1)
                 return true;
             else return false;
         }
